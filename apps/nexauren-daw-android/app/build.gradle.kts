905af5d749d73bb26cd30a88f9bd416ce3ff6e7b
nexaurenstore-source <nexaurenstore@gmail.com>
2026-08-30 00:44:56 +0200

    Create Nexauren DAW Android app module

---
+++ apps/nexauren-daw-android/app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.nexauren.daw"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.nexauren.daw"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
