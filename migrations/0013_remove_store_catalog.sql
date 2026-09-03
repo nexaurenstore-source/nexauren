691c0236fe3ba8d82352fc6796f2edc3db6ecdff
nexaurenstore-source <nexaurenstore@gmail.com>
2026-09-03 22:12:51 +0200

    chore: remove obsolete Marketplace migration

--- migrations/0013_remove_store_catalog.sql
+++
@@ -1,4 +0,0 @@
--- Remove the temporary Store catalog from the shared nexauren-db.
--- The real Marketplace catalog belongs to the dedicated nexauren-marketplace D1.
-
-DROP TABLE IF EXISTS store_products;
