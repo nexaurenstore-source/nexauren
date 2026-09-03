61d911f6674db69f3f90cacf6e1840c9dff82141
nexaurenstore-source <nexaurenstore@gmail.com>
2026-09-03 22:09:23 +0200

    chore: remove Marketplace migrations

--- migrations-marketplace/0002_remove_seed_products.sql
+++
@@ -1,3 +0,0 @@
--- Remove any placeholder/seed products from earlier Marketplace catalog setup.
--- The Marketplace must only expose real products created by administration.
-DELETE FROM store_products;
